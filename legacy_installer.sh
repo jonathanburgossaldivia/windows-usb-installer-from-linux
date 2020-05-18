df25c14993a053c6c5a34a1b1437c0bb1c785d6a
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2020-05-17 21:07:49 -0400

    Add files via upload

--- legacy_installer.sh
+++ legacy_installer.sh
@@ -1,27 +1,25 @@
 usb='sdb'
 usb_name='WINDOWS'
 
-#sudo apt install grub-pc-bin grub-efi-ia32-bin grub-efi-amd64-bin parted
-
+echo '### PREPARING ###'
 sudo killall gparted
-
 for p in `sudo ls /dev/${usb}?`; do sudo umount $p; done
 for p in `sudo ls /dev/sd?`; do sudo umount $p; done
 
+echo '### FILESYSTEMS ###'
 sudo /usr/sbin/sfdisk --delete /dev/${usb}
 sudo /sbin/parted /dev/${usb} mklabel msdos --script
 sudo /sbin/parted /dev/${usb} mkpart primary fat32 0% 100%
 
+echo '### PARTITIONS ###'
 sudo mkfs.fat -F32 -v -I -n ${usb_name} /dev/${usb}1
 
+echo '### GRUB INSTALLATION ###'
 #sudo rm -rf /media/*
-
 sudo rm -rf /media/${usb_name} 
 sudo mkdir /media/${usb_name}
-
 sudo mount /dev/${usb}1 /media/${usb_name}/
 sudo grub-install --target=i386-pc --recheck --boot-directory=/media/${usb_name} /dev/${usb}
-
 sudo mkdir -p /media/${usb_name}/grub
 cat << EOF > /media/${usb_name}/grub/grub.cfg
 default=1  
@@ -30,7 +28,7 @@ set menu_color_highlight=yellow/dark-gray
 set menu_color_normal=black/light-gray
 set color_normal=yellow/black
  
-menuentry "Start Windows Installation" {
+menuentry "USB Installation for Microsoft Windows /7/8/8.1/10 MBR/MSDOS" {
     insmod ntfs
     insmod search_label
     search --no-floppy --set=root --label ${usb_name} --hint hd0,msdos1
