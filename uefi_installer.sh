7fc0cb6cfa6cde0ccee2966523cd4accc4c13bd0
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2020-09-25 14:20:23 -0300

    Update uefi_installer.sh

--- uefi_installer.sh
+++ uefi_installer.sh
@@ -1,3 +1,5 @@
+## sudo apt install grub-efi-amd64-bin grub-efi-ia32-bin
+
 usb='sdb'
 usbname='WINDOWS'
 
@@ -8,20 +10,28 @@ for p in `sudo ls /dev/sd?`; do sudo umount $p 2> /dev/null; done
 
 echo "\n\n### FILESYSTEMS ###\n\n"
 sudo /usr/sbin/sfdisk --delete /dev/${usb}
-sudo /sbin/parted /dev/${usb} mklabel msdos --script
+sudo /sbin/parted -s /dev/${usb} mklabel gpt
 
 echo "\n\n### PARTITIONS ###\n\n"
-sudo /sbin/parted /dev/${usb} mkpart primary fat32 0% 100%
-sudo mkfs.fat -F32 -v -I -n ${usbname} /dev/${usb}1
+sudo /sbin/parted /dev/${usb} mkpart primary fat32 0% 10%
+sudo /sbin/parted /dev/${usb} mkpart primary ntfs 10% 100%
+
+sleep 5 #debe estar si o si
+
+sudo mkfs.fat -F32 -I -n EFII /dev/${usb}1
+sudo mkfs.ntfs --quick -F -L ${usbname} /dev/${usb}2
 
 echo "\n\n### GRUB INSTALLATION ###\n\n"
-#sudo rm -rf /media/*
-sudo rm -rf /media/${usbname} 
-sudo mkdir /media/${usbname}
-sudo mount /dev/${usb}1 /media/${usbname}/
-sudo grub-install --removable --boot-directory=/media/${usbname} --efi-directory=/media/${usbname} --target=x86_64-efi /dev/${usb}
-sudo mkdir -p /media/${usbname}/grub
-sudo bash -c "cat << EOF > /media/${usbname}/grub/grub.cfg
+
+sudo rm -rf /media/EFII
+sudo mkdir /media/EFII
+
+sudo mount /dev/${usb}1 /media/EFII/
+
+sudo grub-install --removable --boot-directory=/media/EFII --efi-directory=/media/EFII --target=x86_64-efi /dev/${usb}
+sudo mkdir -p /media/EFII/grub
+
+sudo bash -c "cat << EOF > /media/EFII/grub/grub.cfg
 default=1  
 timeout=15
 set menu_color_highlight=yellow/dark-gray
@@ -30,10 +40,9 @@ set color_normal=yellow/black
  
 menuentry 'USB Installation for Microsoft Windows 7/8/8.1/10 UEFI/GPT' {
     insmod ntfs
-    insmod search_label
-    search --no-floppy --set=root --label ${usbname} --hint hd0,msdos1
-    ntldr /bootmgr
+    set root='hd0,gpt2'
+    chainloader /efi/boot/bootx64.efi
     boot
 }
 EOF"
-sudo cp /media/${usbname}/grub/grub.cfg /media/${usbname}/EFI/BOOT/grub.cfg
+sudo cp /media/EFII/grub/grub.cfg /media/EFII/EFI/BOOT/grub.cfg
